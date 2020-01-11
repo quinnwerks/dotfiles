5233914eb172d84ef0971cbb2decd269d78fe79d
quinnwerks <quinn.smith98@gmail.com>
2020-01-11 18:06:10 -0500

    print help if no args given

--- setup.py
+++ setup.py
@@ -3,25 +3,32 @@
 @author Quinn Smith
 """
 import argparse
+import sys
 from manager import *
-def getArgs():
-    parser = argparse.ArgumentParser(description="Set Up My Terminal Workflow")
-    parser.add_argument("--unlink", dest="unlink", nargs="*", default=[], type=str, help="Unlink dotfiles")
-    parser.add_argument("--update", dest="update", nargs="*", default=[], type=str, help="Update tags")
-    parser.add_argument("--link",   dest="link",   nargs="*", default=[], type=str, help="Link dotfiles")
+def getArgs(parser):
     parser_args = parser.parse_args()
     return_args = {"link":set(parser_args.link), \
                    "unlink":set(parser_args.unlink), \
                    "update":set(parser_args.update)}
     return return_args
 
+def setUpParser():
+    parser = argparse.ArgumentParser(description="Set Up My Terminal Workflow")
+    parser.add_argument("--unlink", dest="unlink", nargs="*", default=[], type=str, help="Unlink dotfiles")
+    parser.add_argument("--update", dest="update", nargs="*", default=[], type=str, help="Update tags")
+    parser.add_argument("--link",   dest="link",   nargs="*", default=[], type=str, help="Link dotfiles")
+    return parser
+
 
 def main():
     logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
-    args = getArgs()
-    print(args)
-    manager = Manager()
-    manager.run(args)
+    parser = setUpParser()
+    args = getArgs(parser)
+    if(len(sys.argv)==1):
+        parser.print_help(sys.stderr)
+    else:
+        manager = Manager()
+        manager.run(args)
 
 
 
