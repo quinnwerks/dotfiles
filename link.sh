27b720f09076de05b7d49447b105188400cf2beb
quinnwerks <quinn.smith98@gmail.com>
2020-03-31 21:20:01 -0400

    added simlink functionality to script

---
+++ link.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+for DOTFILE_IN_DIR in $DIR/src/* 
+do
+	DOTFILE=$(basename $DOTFILE_IN_DIR)
+	DOTFILE_IN_HOME="$HOME/.$DOTFILE"
+	echo "LINKING $DOTFILE_IN_DIR -> $DOTFILE_IN_HOME"
+	ln -s  $DOTFILE_IN_DIR $DOTFILE_IN_HOME 
+done
