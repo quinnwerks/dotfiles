ba1d712f56ddb9e682b08d17246fc4c511c6fa06
quinnwerks <quinn.smith98@gmail.com>
2020-04-06 21:37:31 -0400

    split up config so it is easier to modify

--- src/xmonad/xmonad.hs
+++ src/xmonad/xmonad.hs
@@ -4,20 +4,34 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Layout.Spacing
+import XMonad.Hooks.EwmhDesktops
 import System.IO
+
+--main::IO()
+--main = xmonad =<< statusBar myBar myPP (ewmh $ myConfig)
 
 main = do
     xmproc <- spawnPipe "xmobar"
-    xmonad $ defaultConfig
-        { manageHook = manageDocks <+> manageHook defaultConfig
-        , handleEventHook = handleEventHook defaultConfig <+> docksEventHook
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        }
-        } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        , ((0, xK_Print), spawn "scrot")
-        ]
+    xmonad $ myConfig
+
+myConfig = defaultConfig { layoutHook = myLayout,
+                           manageHook = myManage,
+                           handleEventHook = myEvent
+                         } `additionalKeys` myKeys
+
+
+myManage = manageDocks <+> manageHook defaultConfig
+myEvent = handleEventHook defaultConfig <+> docksEventHook
+myLayout = avoidStruts  $  layoutHook defaultConfig
+
+
+myBar = "xmobar"
+myPP = dynamicLogWithPP xmobarPP { 
+                    ppCurrent = xmobarColor "blue" ""
+                    ,ppTitle = xmobarColor "green" "" . shorten 80
+                }
+
+
+myKeys = []
+
