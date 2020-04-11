2075bafc504ca99427b627ea61fba385a10720d6
quinnwerks <quinn.smith98@gmail.com>
2020-04-11 11:17:52 -0400

    Get weather and time formatted

--- src/xmobar/xmobar.hs
+++ src/xmobar/xmobar.hs
@@ -1,4 +1,4 @@
-Config {font = "xft:Sans Serif-10:normal,FontAwesome: size=14"  
+Config {font = "xft:Sans Serif-10:normal,FontAwesome: size=11"  
         bgColor = "black",
         fgColor = "grey",
         position = TopW L 100, 
@@ -9,7 +9,8 @@ Config {font = "xft:Sans Serif-10:normal,FontAwesome: size=14"
                     Run Com "bash" ["-c", "~/.scripts/xmobar/volume"]  "vol" 10,
                     Run Com "bash" ["-c", "~/.scripts/xmobar/wifi"]    "wifi" 10,
                     Run Com "bash" ["-c", "~/.scripts/xmobar/battery"] "bat" 10,
-                    Run Weather "RJTT" [ "--template", "<fc=#4682B4><tempC></fc>°C/<fc=#4682B4><rh></fc>%"] 36000
+                    Run Com "bash" ["-c", "~/.scripts/xmobar/time"]    "time" 10,
+                    Run Com "bash" ["-c", "~/.scripts/xmobar/weather"] "weather" 600
                    ],
-        template = "%StdinReader% }{ %bat% %vol% | Wifi:%wifi% | %date% | %RJTT%"
+        template = "%StdinReader% }{ %bat%   %vol%   %wifi%    %weather%    %time%  "
        }
